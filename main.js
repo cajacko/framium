0fc35f4690fe26a229749994053671c7d1512a34
Charlie Jackson <charlie@charliejackson.com>
2018-03-13 17:21:24 +0000

    Added electron builder

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const path = require("path");
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ kiosk: true });
+  // win = new BrowserWindow({ kiosk: true });
+  win = new BrowserWindow({});
 
   // and load the index.html of the app.
   win.loadURL("https://dev.charliejackson.com");
