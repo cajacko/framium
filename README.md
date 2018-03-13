5ec584cdde29e9db8e0f9b10753883506940139e
Charlie Jackson <charlie@charliejackson.com>
2018-03-13 18:07:42 +0000

    Show in full screen

---
+++ README.md
@@ -0,0 +1,1 @@
+* Use electron v1.7.9. 1.8 didn't work on pi
--- main.js
+++ main.js
@@ -6,16 +6,17 @@ var win;
 
 function createWindow() {
   // Create the browser window.
-  // win = new BrowserWindow({ kiosk: true });
-  win = new BrowserWindow({});
+  win = new BrowserWindow({
+    kiosk: true,
+    alwaysOnTop: true,
+    fullscreen: true,
+    frame: false,
+    autoHideMenuBar: true,
+    titleBarStyle: "hidden"
+  });
 
   // and load the index.html of the app.
   win.loadURL("https://dev.charliejackson.com");
-
-  // win.webContents.on("did-fail-load", function(event) {
-  //   console.log("fail");
-  //   console.log(event);
-  // });
 }
 
 app.on("ready", createWindow);
