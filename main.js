a8d149edc9144db034d9a13db1d7cb72b3458a77
Charlie Jackson <charlie@charliejackson.com>
2018-03-13 17:55:34 +0000

    Changed electron version

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const { app, BrowserWindow } = require("electron");
 const url = require("url");
 const path = require("path");
 
+var win;
+
 function createWindow() {
   // Create the browser window.
   // win = new BrowserWindow({ kiosk: true });
@@ -9,6 +11,11 @@ function createWindow() {
 
   // and load the index.html of the app.
   win.loadURL("https://dev.charliejackson.com");
+
+  // win.webContents.on("did-fail-load", function(event) {
+  //   console.log("fail");
+  //   console.log(event);
+  // });
 }
 
 app.on("ready", createWindow);
